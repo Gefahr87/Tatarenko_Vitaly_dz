fcb01fd34b2422dbad69d7f85384a21cde941e4b
ViTat <Vitaly.Gef@gmail.com>
2022-04-13 18:07:06 +0300

    Task 2 almost comleted

---
+++ Task_2/Task_2_1.py
@@ -0,0 +1,16 @@
+# Ну, начнём-с
+'''
+Выяснить тип результата выражений:
+15 * 3
+15 / 3
+15 // 2
+15 ** 2
+'''
+
+mlt_plication = 15 * 3
+division = 15 / 3
+integer_division = 15 // 3
+remndr_division = 15 % 3
+
+print(f'Тип при операции 15*3: {type(mlt_plication)}', f'Тип при операции 15/3: {type(division)}',
+      f'Тип при операции 15//2: {type(integer_division)}', f'Тип при операции 15%2: {type(remndr_division)}', sep='\n')
