10bc20c7c1cdca76668aebd9965d79b3a5d65ff9
ViTat <Vitaly.Gef@gmail.com>
2022-10-01 18:34:55 +0300

    Algorithms Task 1 started

--- DataStructures_Algorithms/Lesson_1/task_2.py
+++ DataStructures_Algorithms/Lesson_1/task_2.py
@@ -16,15 +16,23 @@
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
 
-def min_Olinear (lst_obj: list) -> 'dict of index & value min element':
-    res = dict.fromkeys(['index_min_el', 'value_min_el', 'sorted_lst'])
-    res['value_min_el'] = lst_obj[0]
-    for i in range(len(lst_obj) - 1):
-        if lst_obj[i] < res['value_min_el']:
-            res['index_min_el'] = i
-            res['value_min_el'] = lst_obj[i]
-    return res.items()
+def min_o_linear (lst_obj: list) -> 'dict of index & value min element':    # O(n)
+    res = dict.fromkeys(['index_min_el', 'value_min_el'])                   # O(1)
+    res['value_min_el'] = lst_obj[0]                                        # O(1)
+    for i in range(len(lst_obj)):                                           # n + n + n
+        if lst_obj[i] < res['value_min_el']:                                # O(1)
+            res['index_min_el'] = i                                         # O(1)
+            res['value_min_el'] = lst_obj[i]                                # O(1)
+    return res                                                              # O(1)
 
+def min_o_quater (lst_obj: list) -> 'dict of index & value min element':    # O(n^2)
+    number_of_rep = len(lst_obj)                                            # n + n
+    for _ in range(number_of_rep):                                          # n
+        for i in range(number_of_rep-1):                                    # n
+            if lst_obj[i] > lst_obj[i+1]:                                   # 1
+                lst_obj[i], lst_obj[i+1] = lst_obj[i+1], lst_obj[i]         # 1
+    return lst_obj                                                          # 1
 
 if __name__ == '__main__':
-    print(min_Oquadr([9, 5, 7, 8, 3, 1, 10, 4]))
+    print(min_o_linear([9, 5, 7, 8, 3, 4, 10, 1])['value_min_el'])
+    print(min_o_quater([9, 5, 7, 8, 3, 4, 10, 1])[0])
--- DataStructures_Algorithms/Lesson_1/task_3.py
+++ DataStructures_Algorithms/Lesson_1/task_3.py
@@ -17,3 +17,27 @@
 Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
 """
 
+def version_1 (dict_of_company: dict) -> list:                                  # O(n)
+    max_val, max_val_key = [], []                                               # O(1)
+    i = 0                                                                       # 0(1)
+    while i < 3:                                                                # 0(n) n = 3
+        max_val.append(max(dict_of_company.values()))                           # O(n) + O(n) + O(1)
+        max_val_key.append(max(dict_of_company, key=dict_of_company.get))       # O(n)
+        del dict_of_company[max_val_key[-1]]                                    # O(1)
+        i += 1                                                                  # O(1)
+    return list(zip(max_val, max_val_key))                                      # O(1)
+
+def version_2(dict_of_company: dict) -> dict:                                   # O(n^2)
+    sorted_value = sorted(dict_of_company.values(), reverse=True)               # O(n log n)
+    sorted_dict_of_3_company = {}                                               # O(1)
+    for val in sorted_value[:3]:                                                # O(n^2)
+        for key in dict_of_company.keys():                                      # O(n)
+            if dict_of_company[key] == val:                                     # O(1)
+                sorted_dict_of_3_company[key] = val                             # O(1)
+    return sorted_dict_of_3_company                                             # O(1)
+
+if __name__ == '__main__':
+    profit_of_company = {'company_1': 1546879, 'company_2': 852316, 'company_3': 55122231, 'company_3': 46546132,
+                         'company_4': 46951326, 'company_5': 6461564}
+    print(version_2(profit_of_company))
+    print(version_1(profit_of_company))
