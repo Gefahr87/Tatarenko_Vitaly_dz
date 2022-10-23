224802a9e0e91e23bbb2d6d2ff0525e4f90becbe
ViTat <Vitaly.Gef@gmail.com>
2022-10-23 18:03:58 +0300

    Algorithms Task 3 is done

--- DataStructures_Algorithms/Lesson_3/task_4.py
+++ DataStructures_Algorithms/Lesson_3/task_4.py
@@ -18,36 +18,33 @@
 import hashlib
 
 
+def create_hash_sha512(obj: str):
+    return hashlib.pbkdf2_hmac(hash_name='sha512',
+                        password=obj.encode(),
+                        salt=obj[8:].encode(),
+                        iterations=100000).hex()
+
+
 class CashingWebUrl:
     known_host = dict()
     def __init__(self, url):
         self.url = url
-        self.hash = (hashlib.pbkdf2_hmac(hash_name='sha512',
-                                        password=url.encode(),
-                                        salt=url[8:].encode(),
-                                        iterations=100000)).hex()
+        self.hash = create_hash_sha512(url)
         CashingWebUrl.known_host.update({self.url: self.hash})
+
 
 def check_known_url(url_to_check: str):
     if url_to_check in CashingWebUrl.known_host:
-        return CashingWebUrl[url_to_check]
+        return CashingWebUrl.known_host[url_to_check]
     else:
-        return (hashlib.pbkdf2_hmac(hash_name='sha512',
-                                         password=url_to_check.encode(),
-                                         salt=url_to_check[8:].encode(),
-                                         iterations=100000)).hex()
-
+        CashingWebUrl.known_host.update({url_to_check: create_hash_sha512(url_to_check)})
+        return f'Новый {url_to_check=} был добавлен в список известных адресов.\n' \
+               f'Хэш: {CashingWebUrl.known_host[url_to_check]}'
 
 
 if __name__ == '__main__':
-    yandex = CashingWebUrl('https://yandex.ru')
-    yandex_map = CashingWebUrl('https://yandex.ru/maps/')
-    print(CashingWebUrl.known_host)
-    print(CashingWebUrl.known_host['https://yandex.ru'])
-    check_known_url('https://yandex.ru')
-
-
-
-
-
+    CashingWebUrl('https://yandex.ru')
+    CashingWebUrl('https://yandex.ru/maps/')
+    CashingWebUrl('https://www.gismeteo.ru')
+    print(check_known_url('https://www.metro.ru'))
 
