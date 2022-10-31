e7cad4f36c478f3b51b26537733bc7b7d79a7de4
ViTat <Vitaly.Gef@gmail.com>
2022-10-31 17:51:17 +0300

    Algorithms Task 4 in progress

--- DataStructures_Algorithms/Lesson_4/task_3.py
+++ DataStructures_Algorithms/Lesson_4/task_3.py
@@ -11,7 +11,7 @@
 
 Сделайте вывод, какая из четырех реализаций эффективнее и почему!
 """
-
+from timeit import timeit
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
@@ -35,3 +35,25 @@ def revers_3(enter_num):
     enter_num = str(enter_num)
     revers_num = enter_num[::-1]
     return revers_num
+
+
+def revers_4(enter_num, revers_num=0):
+    for _ in range(len(str(enter_num))):
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+if __name__ == "__main__":
+    number_to_reverse = 4564123789
+    print(timeit('revers(number_to_reverse)', globals=globals(), number=10000))
+    print(timeit('revers_2(number_to_reverse)', globals=globals(), number=10000))
+    print(timeit('revers_3(number_to_reverse)', globals=globals(), number=10000))
+    print(timeit('revers_4(number_to_reverse)', globals=globals(), number=10000))
+
+'''
+Вывод: самый быстрый метод оказался строковый, так как его сложность константная, когда все остальные имеют линейную, 
+а реверсивный вариант экспоненциальную. По сути мы не производим вичислений, а лишь обращаемся к ячейкам памяти в 
+нужном порядке. 
+'''
