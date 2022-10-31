27e313e27cb69905e2d6ff3549a7097a3f8bb8dd
ViTat <Vitaly.Gef@gmail.com>
2022-10-31 16:52:41 +0300

    Algorithms Task 4 in progress

--- DataStructures_Algorithms/Lesson_4/task_1.py
+++ DataStructures_Algorithms/Lesson_4/task_1.py
@@ -11,7 +11,7 @@
 
 ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
 """
-from timeit import Timer
+from timeit import timeit
 
 def func_1(nums):
     new_arr = []
@@ -21,6 +21,19 @@ def func_1(nums):
     return new_arr
 
 
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
 if __name__ == '__main__':
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    print(Timer(stmt='func_1(nums)', setup="from __main__ import func_1", numbers=10000000))
+    nums = list(range(200))
+    print(f'Результатат {func_1(nums)=},\n'
+          f'время на выполнение: ', timeit('func_1(nums)', globals=globals(), number=100000))
+    print(f'Результатат {func_2(nums)=},\n'
+          f'время на выполнение: ', timeit('func_2(nums)', globals=globals(), number=100000))
+
+'''
+Вывод: Для сокращения времени выполнения программы был реализован List Comprehension. При малом объёме 
+элементов(20) в массиве nums, выиграш около 5%. По мере увеличения количества элементов (200) это процент так же растёт
+и достигает уже 20%. 
+'''
