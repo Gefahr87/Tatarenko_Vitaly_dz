7a82b6e2a0bc1db0da4dac2145cdd221c86e9c3a
ViTat <Vitaly.Gef@gmail.com>
2022-11-07 18:59:08 +0300

    Algorithms Task_5 started

--- DataStructures_Algorithms/Lesson_5/task_1.py
+++ DataStructures_Algorithms/Lesson_5/task_1.py
@@ -32,14 +32,19 @@ from collections import Counter
 
 
 class Company:
+    amount_of_companies = 0
+    common_profit = 0
 
     def __init__(self, name: str, lst_of_profit: list):
         self.name = name
         self.quart_profit = lst_of_profit
-        self.avg_profit = sum(lst_of_profit) / len(lst_of_profit)
+        self.year_profit = sum(lst_of_profit)
+        Company.amount_of_companies += 1
+        Company.common_profit += self.year_profit
 
 
 if __name__ == '__main__':
+    n = 0
     comp = []
     # amount_of_company = int(input('Введите количество предприятий для расчета прибыли: '))
     # for number_company in range(amount_of_company):
@@ -47,8 +52,17 @@ if __name__ == '__main__':
     #     quart_profit = list(map(int, input('через пробел введите прибыль данного предприятия '
     #                          'за каждый квартал(Всего 4 квартала): ').split()))
     #     comp.append(Company(name, quart_profit))
-    comp.append(Company('Awesome', [11, 56, 56, 11]))
-    comp.append(Company('Great', [55, 44, 33, 22]))
-    counr_obj = Counter({cmp.name: cmp.avg_profit for cmp in comp}).most_common()
-    print((counr_obj[0][1] + counr_obj[1][1]) / 2)
+    comp.append(Company('Awesome', [235, 345634, 55, 235]))
+    comp.append(Company('Great', [345, 34, 543, 34]))
+    comp.append(Company('North', [213405, 15434, 5463, 30264]))
+    comp.append(Company('South', [3545, 364, 5843, 340]))
+    comp.append(Company('West', [13231, 304, 5453, 3499]))
+    countr_obj = Counter({cmp.name: cmp.year_profit for cmp in comp})
+    print(countr_obj)
+    avg_profit_of_companies = countr_obj.total() / Company.amount_of_companies
+    above_avarage = {cmp.name for cmp in comp if cmp.year_profit > avg_profit_of_companies}
+    below_avarage = {cmp.name for cmp in comp if cmp.year_profit < avg_profit_of_companies}
+    print(f'Средняя годовая прибыль всех предприятий: {avg_profit_of_companies}')
+    print(f'Предприятия, с прибылью выше среднего значения: {above_avarage}')
+    print(f'Предприятия, с прибылью ниже среднего значения: {below_avarage}')
 
