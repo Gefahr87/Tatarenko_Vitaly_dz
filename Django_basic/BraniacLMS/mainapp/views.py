7bf0d15bba6702fa76b4c7923658989bd14857c6
ViTat <Vitaly.Gef@gmail.com>
2023-03-02 12:37:03 +0300

    Переж добавлением статичного сайта

--- Django_basic/BraniacLMS/mainapp/views.py
+++ Django_basic/BraniacLMS/mainapp/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.views.generic import View
 
 # Create your views here.
 
-def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
 
-def hello_world(request):
-    template = loader.get_template('hello_world.html')
-    return HttpResponse(template.render())
+class HelloWorldView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Hello worldы!')
+
 
 def check_kwargs(request, **kwrgs):
     return HttpResponse(f"kwargs:<br>{kwrgs}")
