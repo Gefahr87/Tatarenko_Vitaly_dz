5f91a56e08e60ddf3ddd16228c199bdc8b75f4e4
Gefahr87@mail.ru <gefahr87@mail.ru>
2023-03-24 13:56:12 +0300

    Video 0:55

--- Django_basic/BraniacLMS/mainapp/urls.py
+++ Django_basic/BraniacLMS/mainapp/urls.py
@@ -8,11 +8,11 @@ app_name = MainappConfig.name
 urlpatterns = [
     # path('', views.HelloWorldView.as_view()),
     path('', views.MainPageView.as_view()),
-    path('index/', views.MainPageView.as_view()),
-    path('news/', views.NewsPageView.as_view()),
-    path('courses_list/', views.CoursesPageView.as_view()),
-    path('contacts/', views.ContactsPageView.as_view()),
-    path('doc_site/', views.DocSitePageView.as_view()),
-    path('login/', views.LoginPageView.as_view()),
+    path('index.html/', views.MainPageView.as_view()),
+    path('news.html/', views.NewsPageView.as_view()),
+    path('courses_list.html/', views.CoursesPageView.as_view()),
+    path('contacts.html/', views.ContactsPageView.as_view()),
+    path('doc_site.html/', views.DocSitePageView.as_view()),
+    path('login.html/', views.LoginPageView.as_view()),
     path('<str:word>/', views.check_kwargs),
 ]
