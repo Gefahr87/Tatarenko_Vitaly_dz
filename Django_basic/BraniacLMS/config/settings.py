8bdb0787f1294e2bdea07be440f50324815c9c24
ViTat <Vitaly.Gef@gmail.com>
2023-04-11 19:44:35 +0300

    Video 39.42 add msql_config

--- Django_basic/BraniacLMS/config/settings.py
+++ Django_basic/BraniacLMS/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from mysql_config import db_host, db_user, db_password, db_name
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
